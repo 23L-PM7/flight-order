b5580134ae47072c0ebb99a21ce74303dbe361db
temvvlen11 <151615708+temvvlen11@users.noreply.github.com>
2024-05-02 19:27:41 +0800

    .

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,22 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
-
-
-
-
   return (
-    <footer className="flex w-full h-[422px] flex-col items-center justify-center bg-[#8DD3BB] mt-[300px]">
-      <div className="w-10/12 gap-16 flex flex-col -translate-y-24">
-        <div className="flex justify-between rounded-3xl max-[1210px]:justify-center bg-[#CDEAE1] px-6">
+    <footer className="mt-[300px] flex h-[422px] w-full flex-col items-center justify-center bg-[#8DD3BB]">
+      <div className="flex w-10/12 -translate-y-24 flex-col gap-16">
+        <div className="flex justify-between rounded-3xl bg-[#CDEAE1] px-6 max-[1210px]:justify-center">
           <div className="flex flex-col justify-between py-6 max-[1210px]:items-center">
-            <p className="flex-col font-mono opacity-80 text-5xl font-semibold max-[1210px]:justify-center text-black">
+            <p className="flex-col font-mono text-5xl font-semibold text-black opacity-80 max-[1210px]:justify-center">
               Subscribe
               <br />
               Newsletter
             </p>
             <div className="flex flex-col gap-4">
-              <div className="gap-2 max-[1210px]:align-middle max-[1210px]:flex-col max-[1210px]:items-center max-[1210px]:flex">
+              <div className="gap-2 max-[1210px]:flex max-[1210px]:flex-col max-[1210px]:items-center max-[1210px]:align-middle">
                 <p className="text-xl font-bold text-[#112211] opacity-80">
                   The Travel
                 </p>
@@ -30,7 +27,7 @@ const Footer = () => {
                   className="w-full rounded px-2 placeholder:font-sans placeholder:text-base placeholder:font-light placeholder:leading-5 placeholder:text-[#1C1B1F] placeholder:opacity-80"
                   placeholder="Your email address"
                 ></input>
-                <button className="rounded text-white delay-75 ease-in-out duration-100 bg-[#112211] px-4 py-2 font-mono text-sm hover:bg-gray-400">
+                <button className="rounded bg-[#112211] px-4 py-2 font-mono text-sm text-white delay-75 duration-100 ease-in-out hover:bg-gray-400">
                   Subscribe
                 </button>
               </div>
@@ -43,14 +40,14 @@ const Footer = () => {
         <div className="flex gap-[140px] max-[1210px]:justify-center">
           <div className="flex flex-col gap-6 max-[1210px]:items-center">
             <img src="./hm.png" alt="logo"></img>
-            <div className="flex gap-2 max-[1210px]:gap-4">
+            <Link href="/" className="flex gap-2 max-[1210px]:gap-4">
               <img src="./facebook.svg" alt="facebook" />
               <img src="./twitter.svg" alt="twitter" />
               <img src="./youtube.svg" alt="youtube" />
               <img src="./instagram.svg" alt="instagram" />
-            </div>
+            </Link>
           </div>
-          <div className="mr-20 flex w-full max-[1210px]:hidden justify-between gap-6">
+          <div className="mr-20 flex w-full justify-between gap-6 max-[1210px]:hidden">
             <div className="flex flex-col gap-4 text-[#112211]">
               <div className="font-sans text-base font-semibold">
                 <h1 className="">Our Destinations</h1>
