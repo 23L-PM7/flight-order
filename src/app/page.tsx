2d815e37707e333b874b414d42f7f48dd7a3d2a3
Uranzul1005 <151522613+Uranzul1005@users.noreply.github.com>
2024-05-03 18:31:24 +0800

    delete header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="bg-[#FAFBFC] relative">
       <Hero />
-      <div className=" absolute top-[80%] left-[17%]">
+      <div className=" absolute top-[80%] left-[17%] ">
         <FindFlight />
       </div>
     </main>
