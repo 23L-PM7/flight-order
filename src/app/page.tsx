a3971fcea399587bc4142c782e7c88eec410686e
Uranzul1005 <151522613+Uranzul1005@users.noreply.github.com>
2024-05-03 18:28:04 +0800

    delete header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Header } from "../components/header/Header";
 import { Hero } from "../components/home/Hero";
 import { FindFlight } from "../components/home/FindFlight";
 
 export default function Home() {
   return (
     <main className="bg-[#FAFBFC] relative">
-      <Header />
       <Hero />
       <div className=" absolute top-[80%] left-[17%]">
         <FindFlight />
