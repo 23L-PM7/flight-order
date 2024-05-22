2e2e5a8243455c2636478cef457d68bbe316ef55
temvvlen1 <164151674+temvvlen1@users.noreply.github.com>
2024-05-22 16:22:41 +0800

    d

--- src/app/flightlist/components/FlightList.tsx
+++ src/app/flightlist/components/FlightList.tsx
@@ -5,27 +5,6 @@ import ShowResult from "./ShowResult";
 const FlightList = () => {
   const [flightInfo, setFlightInfo]: any = useState<any[]>([]);
 
-  // useEffect(() => {
-  //   const getFlightData = async () => {
-  //     try {
-  //       const response = await fetch("/api/flightDatas");
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch flight information");
-  //       }
-
-  //       const data = await response.json();
-  //       console.log("data ==>", data)
-  //       setFlightInfo(data);
-  //     } catch (error) {
-  //       console.error("Error fetching flight information:", error);
-  //     }
-  //   };
-
-  //   getFlightData();
-
-  //   return () => {};
-  // }, []);
-
   useEffect(() => {
     const getFlightData = async () => {
       try {
