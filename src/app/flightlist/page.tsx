0a7dc467e5779695ddacffa6a06371bda620fc7c
Uranzul1005 <151522613+Uranzul1005@users.noreply.github.com>
2024-05-23 19:09:56 +0800

    fixed error

--- src/app/flightlist/page.tsx
+++ src/app/flightlist/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React from "react";
+import { Suspense } from "react";
 
 import FlightList from "./components/FlightList";
 
 export default function Home() {
   return (
-    <>
+    <Suspense>
       <FlightList />
-    </>
+    </Suspense>
   );
 }
